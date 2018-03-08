65a5db553c4d9dde0d7ae8db2b2c6f86343b3454
vidit98 <vidit.goel9816@gmail.com>
2018-03-08 18:40:26 +0530

    Updated distance transform algo

---
+++ Transform.cpp
@@ -0,0 +1,316 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
+#include <cstdio>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+using namespace cv;
+using namespace std;
+
+static const int DIST_SHIFT = 16;
+static const int INIT_DIST0 = (INT_MAX >> 2);
+#define  CV_FLT_TO_FIX(x,n)  cvRound((x)*(1<<(n)))
+#define GRADIENT(x,y)  abs((x - y)*(1<<10))
+namespace trans
+{
+static void
+initTopBottom( Mat& temp, int border )
+{
+    Size size = temp.size();
+    for( int i = 0; i < border; i++ )
+    {
+        int* ttop = temp.ptr<int>(i);
+        int* tbottom = temp.ptr<int>(size.height - i - 1);
+
+        for( int j = 0; j < size.width; j++ )
+        {
+            ttop[j] = INIT_DIST0;
+            tbottom[j] = INIT_DIST0;
+        }
+    }
+}
+
+static void distanceTransform_3x3( const Mat& _src, const Mat& _gray, Mat& _temp, Mat& _dist, const float* metrics, int alpha )
+{
+    const int BORDER = 1;
+    int i, j;
+    const int HV_DIST = CV_FLT_TO_FIX( metrics[0], DIST_SHIFT );
+    const int DIAG_DIST = CV_FLT_TO_FIX( metrics[1], DIST_SHIFT );
+    const float scale = 1.f/(1 << DIST_SHIFT);
+
+    const uchar* src = _src.ptr();
+    const uchar* gray = _gray.ptr();
+    int* temp = _temp.ptr<int>();
+    float* dist = _dist.ptr<float>();
+    int srcstep = (int)(_src.step/sizeof(src[0]));
+    int step = (int)(_temp.step/sizeof(temp[0]));
+    int dststep = (int)(_dist.step/sizeof(dist[0]));
+    Size size = _src.size();
+
+    initTopBottom( _temp, BORDER );
+
+    // forward pass
+    for( i = 0; i < size.height; i++ )
+    {
+        const uchar* s = src + i*srcstep;
+        const uchar* g = gray + i*srcstep;
+        int* tmp = (int*)(temp + (i+BORDER)*step) + BORDER;
+
+        for( j = 0; j < BORDER; j++ )
+            tmp[-j-1] = tmp[size.width + j] = INIT_DIST0;
+
+        for( j = 0; j < size.width; j++ )
+        {
+            if( !s[j] )
+                tmp[j] = 0;
+            else
+            {   int a = -step - 1;
+                int t0 = tmp[j-step-1] + DIAG_DIST;
+                int t = tmp[j-step] + HV_DIST;
+                if( t0 > t ) {t0 = t ; a = -step;}
+                t = tmp[j-step+1] + DIAG_DIST ;
+                if( t0 > t ) {t0 = t; a = 1-step;}
+                t = tmp[j-1] + HV_DIST;
+                if( t0 > t ){t0 = t;a = -1;}
+                tmp[j] = t0 + alpha*GRADIENT(g[j],g[j + a]);
+            }
+        }
+    
+    }
+
+    // backward pass
+    for( i = size.height - 1; i >= 0; i--)
+    {
+        float* d = (float*)(dist + i*dststep);
+        int* tmp = (int*)(temp + (i+BORDER)*step) + BORDER;
+        const uchar* g = gray + i*srcstep;
+        for( j = size.width - 1; j >= 0; j-- )
+        {
+            int t0 = tmp[j];
+            int a = 0;
+            if( t0 > HV_DIST )
+            {
+                int t = tmp[j+step+1] + DIAG_DIST;
+                if( t0 > t ) {t0 = t ; a = step + 1;}
+                t = tmp[j+step] + HV_DIST;
+                if( t0 > t ){ t0 = t; a = step;}
+                t = tmp[j+step-1] + DIAG_DIST;
+                if( t0 > t ){ t0 = t; a = step -1;}
+                t = tmp[j+1] + HV_DIST ;
+                if( t0 > t ){ t0 = t; a = 1;}
+                tmp[j] = t0 + alpha*GRADIENT(g[j],g[j + a]);
+            }
+            d[j] = (float)(tmp[j] * scale);
+        }
+    }
+}
+
+static void distanceTransform_5x5( const Mat& _src, const Mat& _gray, Mat& _temp, Mat& _tempg, Mat& _dist, const float* metrics, int alpha)
+{
+    const int BORDER = 2;
+    int i, j;
+    const int HV_DIST = CV_FLT_TO_FIX( metrics[0], DIST_SHIFT );
+    const int DIAG_DIST = CV_FLT_TO_FIX( metrics[1], DIST_SHIFT );
+    const int LONG_DIST = CV_FLT_TO_FIX( metrics[2], DIST_SHIFT );
+    const float scale = 1.f/(1 << DIST_SHIFT);
+
+    const uchar* src = _src.ptr();
+    const uchar* gray = _gray.ptr();
+    int* temp = _temp.ptr<int>();
+    int* tempg = _tempg.ptr<int>();
+    float* dist = _dist.ptr<float>();
+    int srcstep = (int)(_src.step/sizeof(src[0]));
+    int graystep = (int)(_gray.step/sizeof(gray[0]));
+    int step = (int)(_temp.step/sizeof(temp[0]));
+    int stepg = (int)(_tempg.step/sizeof(tempg[0]));
+    int dststep = (int)(_dist.step/sizeof(dist[0]));
+    Size size = _src.size();
+   
+
+    initTopBottom( _temp, BORDER );
+    int a = 0;
+    int c = 0;
+    // forward pass
+    
+    for( i = 0; i < size.height; i++ )
+    {
+        const uchar* s = src + i*srcstep;
+        const uchar* g = gray + i*graystep;
+        int* tmp = (int*)(temp + (i+BORDER)*step) + BORDER;
+        int* tmpg = (int*)(tempg + (i+BORDER)*stepg) + BORDER;
+        tmpg[0] = 1;
+        for( j = 0; j < BORDER; j++)
+        {
+            tmp[-j-1] = tmp[size.width + j] = INIT_DIST0;
+             
+            tmpg[-j-1] = tmpg[size.width + j] = 0;
+            
+        }
+        
+
+        for( j = 0; j < size.width; j++ )
+        {
+            if( !s[j] )
+            {    
+                tmp[j] = 0;
+                tmpg[j] = 0;
+            }
+            else
+            {
+                a = -step*2-1;
+                int t0 = tmp[j-step*2-1] + LONG_DIST ;
+                int t = tmp[j-step*2+1] + LONG_DIST ;
+                if( t0 > t ){t0 = t; a = -graystep*2+1;c = -stepg*2+1;}
+                t = tmp[j-step-2] + LONG_DIST ;
+                if( t0 > t ){ t0 = t ; a = -graystep -2;c = -stepg*-2;}
+                t = tmp[j-step-1] + DIAG_DIST ;
+                if( t0 > t ){t0 = t; a = -graystep -1;c = -stepg-1;}
+                t = tmp[j-step] + HV_DIST;
+                if( t0 > t ){ t0 = t ; a = -graystep;c = -stepg;}
+                t = tmp[j-step+1] + DIAG_DIST;
+                if( t0 > t ){ t0 = t; a = -graystep+1;c = -stepg+1;}
+                t = tmp[j-step+2] + LONG_DIST;
+                if( t0 > t ){ t0 = t; a = -graystep+2;c = -stepg+2;}
+                t = tmp[j-1] + HV_DIST;
+                if( t0 > t ){t0 = t; a = -1;c = -1;}
+                tmp[j] = t0;//temp[j+a] + alpha*GRADIENT(g[j] , g[j + a]);
+                tmpg[j] = tmpg[j+c] + GRADIENT(g[j] , g[j + a]);
+            }
+        }
+    }
+    int b = a;
+    // backward pass
+    for( i = size.height - 1; i >= 0; i-- )
+    {
+        float* d = (float*)(dist + i*dststep);
+        int* tmp = (int*)(temp + (i+BORDER)*step) + BORDER;
+        int* tmpg = (int*)(tempg + (i+BORDER)*stepg) + BORDER;
+        const uchar* g = gray + i*graystep;
+        for( j = size.width - 1; j >= 0; j-- )
+        {
+            int t0 = tmp[j];
+           
+            if( t0 > HV_DIST )
+            {
+                int t = tmp[j+step*2+1] + LONG_DIST;
+
+                if( t0 > t ){t0 = t; a = graystep*2 + 1;c = stepg*2 + 1;}
+                t = tmp[j+step*2-1] + LONG_DIST ;
+                if( t0 > t ){t0 = t;a = graystep*2 -1;c = stepg*2 - 1;}
+                t = tmp[j+step+2] + LONG_DIST ;
+                if( t0 > t ){ t0 = t; a = graystep + 2;c = stepg + 2;}
+                t = tmp[j+step+1] + DIAG_DIST;
+                if( t0 > t ) {t0 = t; a = graystep + 1;c = stepg + 1;}
+                t = tmp[j+step] + HV_DIST ;
+                if( t0 > t ){ t0 = t; a = graystep;c = stepg;} 
+                t = tmp[j+step-1] + DIAG_DIST;
+                if( t0 > t ){ t0 = t; a = graystep -1;c = stepg - 1;}
+                t = tmp[j+step-2] + LONG_DIST ;
+                if( t0 > t ) {t0 = t; a = graystep-2;c = stepg - 2;}
+                t = tmp[j+1] + HV_DIST ;
+                if( t0 > t ){ t0 = t; a = 1; c = 1;}
+                tmp[j] = t0;//temp[j+a]+ alpha*GRADIENT(g[j] , g[j+a]);
+                if(b != a);
+                    tmpg[j] = tmpg[j+c] + GRADIENT(g[j] , g[j + a]);
+               
+            }
+            if (tmpg[j]*scale > 256)
+            {
+                printf("%s\n", "check");
+                printf("%d\n", tmpg[j]*scale);
+            }
+            d[j] = (float)((t0 + alpha*tmpg[j])* scale);
+        }
+    }
+}
+
+
+static void getDistanceTransformMask( int maskType, float *metrics )
+{
+    CV_Assert( metrics != 0 );
+
+    switch (maskType)
+    {
+    case 30:
+        metrics[0] = 1.0f;
+        metrics[1] = 1.0f;
+        break;
+
+    case 31:
+        metrics[0] = 1.0f;
+        metrics[1] = 2.0f;
+        break;
+
+    case 32:
+        metrics[0] = 0.955f;
+        metrics[1] = 1.3693f;
+        break;
+
+    case 50:
+        metrics[0] = 1.0f;
+        metrics[1] = 1.0f;
+        metrics[2] = 2.0f;
+        break;
+
+    case 51:
+        metrics[0] = 1.0f;
+        metrics[1] = 2.0f;
+        metrics[2] = 3.0f;
+        break;
+
+    case 52:
+        metrics[0] = 1.0f;
+        metrics[1] = 1.4f;
+        metrics[2] = 2.1969f;
+        break;
+    default:
+        CV_Error(CV_StsBadArg, "Unknown metric type");
+    }
+}
+
+
+void distanceTransform( InputArray _src, InputArray _gray, OutputArray _dst,
+                            int distType, int maskSize , int alpha)
+{
+   
+    Mat gray = _gray.getMat();
+    Mat src = _src.getMat(), labels;
+    
+    CV_Assert( src.type() == CV_8UC1);
+
+    _dst.create( src.size(), CV_32F);
+    Mat dst = _dst.getMat();
+
+    float _mask[5] = {0};
+    if( maskSize != CV_DIST_MASK_3 && maskSize != CV_DIST_MASK_5 && maskSize != CV_DIST_MASK_PRECISE )
+        CV_Error( CV_StsBadSize, "Mask size should be 3 or 5 or 0 (precise)" );
+
+    CV_Assert( distType == CV_DIST_C || distType == CV_DIST_L1 || distType == CV_DIST_L2 );
+
+    getDistanceTransformMask( (distType == CV_DIST_C ? 0 :
+        distType == CV_DIST_L1 ? 1 : 2) + maskSize*10, _mask );
+
+    Size size = src.size();
+
+    int border = maskSize == CV_DIST_MASK_3 ? 1 : 2;
+    Mat temp( size.height + border*2, size.width + border*2, CV_32SC1 );
+    Mat tempg( size.height + border*2, size.width + border*2, CV_32SC1 );
+    if( maskSize == CV_DIST_MASK_3 )
+    {
+        distanceTransform_3x3(src, gray, temp, dst, _mask, alpha);
+    }
+    else
+    {
+        distanceTransform_5x5(src, gray, temp,tempg, dst, _mask,alpha);
+    }
+
+}
+
+
+}
+// int main()
+// {
+//     return 0;
+// }
--- watershed.cpp
+++ watershed.cpp
@@ -3,160 +3,300 @@
 #include "opencv2/core/core.hpp"
 #include <cstdio>
 #include <iostream>
+#include <set>
+#include "Transform.cpp"
 
 using namespace cv;
 using namespace std;
 
+int alpha = 2 , beta = 155 , c = 7 , s = 1 , s1 = 18;
+
+void sharp_image(Mat img, Mat& output)
+{
+    // do the laplacian filtering as it is
+    // well we need to convert everything in something more deeper then CV_8U
+    // because the kernel hsas some negative values,
+    // and we can expect in general to have a Laplacian image with negative values
+    // BUT a 8bits unsigned int (the one we are working with) can contain values from 0 to 255
+    // so the possible negative number will be truncated
+    
+    Mat imgLaplacian, sharp1, imgResult;
+    Mat kernel = (Mat_<float>(3,3) <<
+            1,  1, 1,
+            1, -8, 1,
+            1,  1, 1);
+
+    filter2D(img, imgLaplacian, CV_32F, kernel);
+    img.convertTo(sharp1, CV_32F);
+    imgResult = sharp1 - imgLaplacian;
+    // convert back to 8bits gray scale
+    imgResult.convertTo(imgResult, CV_8UC3);
+    imgLaplacian.convertTo(imgLaplacian, CV_8UC3);
+
+    output = imgResult; // copy back
+    
+}
+void border(Mat& img)
+{
+    for (int i = 0; i < img.rows; ++i)
+    {
+        img.at<uchar>(i, 0) = img.at<uchar>(i,img.cols -1) = 0; 
+    }
+
+    for (int i = 0; i < img.cols; ++i)
+    {
+        img.at<uchar>(0,i) = img.at<uchar>(img.rows - 1, i) = 0;
+    }
+}
+
+void get_markers(Mat img, Mat& output, int s1, int c, int alpha, int beta, int s, int& size, int flag)
+{
+    Mat bw,bw1,bw2;
+    Mat src_copy = img.clone();
+    cvtColor(img, bw1, CV_BGR2GRAY);
+       
+    medianBlur(bw1,bw1,3);
+    imshow("q" , bw1);
+    imwrite("bn1.jpg",bw1);
+    bilateralFilter(bw1, bw, 2*alpha + 1, beta, 50 );
+    
+    imshow("Binary Image1", bw);
+    imwrite("a.jpg" , bw);
+    // threshold(bw, bw2, 40, 255, CV_THRESH_BINARY_INV| CV_THRESH_OTSU);
+    adaptiveThreshold(bw,  bw2, 255, ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY_INV, 2*s1 + 1, c);
+    imshow("Binary Image", bw2);
+    imwrite("a1.jpg" , bw2);
+    // Perform the distance transform algorithm
+    Mat dist(img.rows, img.cols, CV_8UC1,Scalar(0));
+    //distanceTransform(bw2,dist, CV_DIST_L2, 5);
+    trans::distanceTransform(bw2,bw1,dist, CV_DIST_L2, 5,s);
+    //  Normalize the distance image for range = {0.0, 1.0}
+    //so we can visualize and threshold it
+    normalize(dist, dist, 0, 1., NORM_MINMAX);
+    imshow("Distance Transform Image", dist);
+    imwrite("Distancet.jpg", dist);
+    // Threshold to obtain the peaks
+    // This will be the markers for the foreground objects
+    threshold(dist, dist, .4, 1., CV_THRESH_BINARY);
+    // Dilate a bit the dist image
+    Mat kernel1 = Mat::ones(3, 3, CV_8UC1);
+    if(flag)
+        dilate(dist, dist, kernel1);
+    imshow("Peaks", dist);
+    imwrite("Peaks.jpg", dist);
+   
+    Mat dist_8u;
+    dist.convertTo(dist_8u, CV_8U);
+    // Find total markers
+    vector<vector<Point> > contours;
+    findContours(dist_8u, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+    // Create the marker image for the watershed algorithm
+    Mat markers = Mat::zeros(dist.size(), CV_32SC1);
+    //Draw the foreground markers
+    for (size_t i = 0; i < contours.size(); i++)
+    {
+       if (contourArea(contours[i]) > (img.rows*img.cols/8000))
+        {
+            drawContours(markers, contours, static_cast<int>(i), Scalar::all(static_cast<int>(i)+1), -1);
+            drawContours(src_copy, contours, static_cast<int>(i), Scalar(0,0,255), -1);
+
+        }
+    }
+    imwrite("markers.jpg", src_copy);
+    circle(markers, Point(5,5), 3, CV_RGB(255,255,255), -1);
+    output = markers;
+    size = contours.size();
+    //Draw the background marker
+    
+    imshow("Marker", src_copy);
+   
+}
+
+void _watershed(Mat src, Mat markers, Mat& output, int size, set<int>& index_arr)
+{
+    watershed(src, markers);
+    Mat mark = Mat::zeros(markers.size(), CV_8UC1);
+    markers.convertTo(mark, CV_8UC1);
+    bitwise_not(mark, mark);
+   
+    vector<Vec3b> colors;
+    for (size_t i = 0; i < size; i++)
+    {
+        int b = 13 + 2*i;// theRNG().uniform(0, 255);
+        int g =  13 + 3*i;//theRNG().uniform(0, 255);
+        int r =  13 + 4*i;//theRNG().uniform(0, 255);
+        colors.push_back(Vec3b((uchar)b, (uchar)g, (uchar)r));
+    }
+    // Create the result image
+    Mat dst = Mat::zeros(markers.size(), CV_8UC3);
+   
+
+    // Fill labeled objects with random colors
+    for (int i = 0; i < markers.rows; i++)
+    {
+        for (int j = 0; j < markers.cols; j++)
+        {
+            int index = markers.at<int>(i,j);
+            if (index > 0 && index <= static_cast<int>(size))
+            {
+                dst.at<Vec3b>(i,j) = colors[index-1];
+                index_arr.insert(index-1);
+               
+            }
+
+            else
+                dst.at<Vec3b>(i,j) = Vec3b(0,0,0);
+        }
+    }
+    // Visualize the final image
+    output = dst;
+    imshow("Final Result", dst);
+    imwrite("final.jpg", dst);
+    waitKey(0);
+   
+}
+
+void get_small_segments(Mat dst, vector<Vec3b> colors,  set<int> index_arr, vector<vector<Point> >& contours)
+{
+
+    Mat bw3(dst.rows, dst.cols, CV_8UC3, Scalar(0,0,0));
+    Mat output(dst.rows, dst.cols, CV_8UC3, Scalar(0,0,0));
+    Mat edge;
+    set<int>::iterator it;
+
+    for (it = index_arr.begin(); it != index_arr.end(); ++it)
+    {   
+        vector<vector<Point> > contours_tmp;
+        Scalar color( rand()&255, rand()&255, rand()&255 );
+        int idx = *it;
+       
+        printf("%d %d\n", idx);
+        inRange(dst, Scalar(colors[idx][0] ,colors[idx][1], colors[idx][2]), Scalar(colors[idx][0] + 2,colors[idx][1] + 2, colors[idx][2] + 2), bw3);
+
+        Mat kernel1 = Mat::ones(3, 3, CV_8UC1);
+        dilate(bw3, bw3, kernel1);
+        border(bw3);
+        // imshow("edge", bw3);      
+        // imshow("edge1", bw3);
+        imwrite("edge1.jpg", bw3);
+        findContours(bw3, contours_tmp, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+        contours.push_back(contours_tmp[0]);
+        drawContours(output, contours_tmp, 0,color, -1);
+        //imshow("dst", output);
+        
+        
+   
+
+        
+    }
+}
+
+
+
 int main(int, char** argv)
 {
     // Load the image
-    Mat src = imread(argv[1]);
+    Mat temp = imread(argv[1]);
     
+    Mat src = temp.clone();
     // Check if everything was fine
-    if (!src.data)
+    if (!temp.data)
         return -1;
-    // Show source image
-    imshow("Source Image", src);
-    // Change the background from white to black, since that will help later to extract
-    // better results during the use of Distance Transform
-    for( int x = 0; x < src.rows; x++ ) {
-      for( int y = 0; y < src.cols; y++ ) {
-          if ( src.at<Vec3b>(x, y) == Vec3b(255,255,255) ) {
-            src.at<Vec3b>(x, y)[0] = 0;
-            src.at<Vec3b>(x, y)[1] = 0;
-            src.at<Vec3b>(x, y)[2] = 0;
-          }
-        }
-    }
-    // Show output image
+    
     imshow("Black Background Image", src);
-    // Create a kernel that we will use for accuting/sharpening our image
+    // Create a kernel that we will use for accuting/sharpening our image4
+    Mat bw,bw1,bw2;
     Mat kernel = (Mat_<float>(3,3) <<
             1,  1, 1,
             1, -8, 1,
             1,  1, 1); // an approximation of second derivative, a quite strong kernel
-    // do the laplacian filtering as it is
-    // well, we need to convert everything in something more deeper then CV_8U
-    // because the kernel has some negative values,
-    // and we can expect in general to have a Laplacian image with negative values
-    // BUT a 8bits unsigned int (the one we are working with) can contain values from 0 to 255
-    // so the possible negative number will be truncated
-    Mat imgLaplacian;
-    Mat sharp = src; // copy source image to another temporary one
-    filter2D(sharp, imgLaplacian, CV_32F, kernel);
-    src.convertTo(sharp, CV_32F);
-    Mat imgResult = sharp - imgLaplacian;
-    // convert back to 8bits gray scale
-    imgResult.convertTo(imgResult, CV_8UC3);
-    imgLaplacian.convertTo(imgLaplacian, CV_8UC3);
-    imshow( "Laplace Filtered Image", imgLaplacian );
-    imshow( "New Sharped Image", imgResult );
-    src = imgResult; // copy back
-    // Create binary image from source image
-    Mat bw,bw1;
-    int alpha = 2 , beta = 155 , acc = 50 , minl = 20;
-    namedWindow("Object Detection",WINDOW_AUTOSIZE);
-    createTrackbar("alpha","Object Detection", &alpha, 50);
-    createTrackbar("beta","Object Detection", &beta, 180);
 
-    while(1)
+
+    
+   /* Mat imgLaplacian;
+   */
+    sharp_image(temp , src);
+    imwrite("sharp.jpg", src);
+    imshow("a" , src);
+    waitKey(0);
+    //Create binary image from source image
+    
+  
+    // namedWindow("Object Detection",WINDOW_AUTOSIZE);
+    // createTrackbar("alpha","Object Detection", &alpha, 50);
+    // createTrackbar("beta","Object Detection", &beta, 180);
+    // createTrackbar("gradient","Object Detection", &s, 100);
+    // createTrackbar("constant","Object Detection", &c, 180);
+    // createTrackbar("size","Object Detection", &s1, 180);
+
+    
+    Mat src_copy = temp.clone();
+    vector<vector<Point> > contours;
+    set<int> index_arr;
+    Mat markers;
+    int size;
+    Mat dst;
+    get_markers(src, markers, s1, c, alpha, beta, s, size, 1);
+
+    vector<Vec3b> colors;
+    
+    for (size_t i = 0; i < size; i++)
     {
-        Mat src_copy = imread(argv[1]);
-
-        cvtColor(src, bw1, CV_BGR2GRAY);
-
-        medianBlur(bw1,bw,3);
-        imshow("q" , bw1);
-        imwrite("bn1.jpg",bw1);
-        bilateralFilter(bw1, bw, 2*alpha + 1, beta, 50 );
-        
-        
-        imwrite("a.jpg" , bw);
-        threshold(bw, bw, 40, 255, CV_THRESH_BINARY_INV| CV_THRESH_OTSU);
-        //adaptiveThreshold(bw,  bw, 255, ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY_INV, 31, 8);
-        imshow("Binary Image", bw);
-        imwrite("a1.jpg" , bw);
-        // Perform the distance transform algorithm
-        Mat dist;
-        distanceTransform(bw, dist, CV_DIST_L2, 3);
-        // Normalize the distance image for range = {0.0, 1.0}
-        // so we can visualize and threshold it
-        normalize(dist, dist, 0, 1., NORM_MINMAX);
-        imshow("Distance Transform Image", dist);
-        imwrite("Distancet.jpg", dist);
-        // Threshold to obtain the peaks
-        // This will be the markers for the foreground objects
-        threshold(dist, dist, .4, 1., CV_THRESH_BINARY);
-        // Dilate a bit the dist image
-        Mat kernel1 = Mat::ones(3, 3, CV_8UC1);
-        dilate(dist, dist, kernel1);
-        imshow("Peaks", dist);
-        imwrite("Peaks.jpg", dist);
-        // Create the CV_8U version of the distance image
-        // It is needed for findContours()
-        Mat dist_8u;
-        dist.convertTo(dist_8u, CV_8U);
-        // Find total markers
-        vector<vector<Point> > contours;
-        findContours(dist_8u, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
-        // Create the marker image for the watershed algorithm
-        Mat markers = Mat::zeros(dist.size(), CV_32SC1);
-        // Draw the foreground markers
-        for (size_t i = 0; i < contours.size(); i++)
-            {
-               // if (contourArea(contours[i]) < 200)
-                {
-                    drawContours(markers, contours, static_cast<int>(i), Scalar::all(static_cast<int>(i)+1), -1);
-                    drawContours(src_copy, contours, static_cast<int>(i), Scalar::all(static_cast<int>(i)+1), -1);
-
-                }
-            }
-
-        // Draw the background marker
-        circle(markers, Point(5,5), 3, CV_RGB(255,255,255), -1);
-        // circle(markers, Point(dist.rows-5,5), 3, CV_RGB(255,255,255), -1);
-        // circle(markers, Point(dist.rows-5,dist.cols -5), 3, CV_RGB(255,255,255), -1);
-        // circle(src_copy, Point(dist.rows-5,5), 3, CV_RGB(0,255,255), -1);
-        // circle(src_copy, Point(dist.rows-5,dist.cols -5), 3, CV_RGB(0,255,255), -1);
-
-        circle(src_copy, Point(5,5), 3, CV_RGB(0,255,255), -1);
-        imshow("Markers", src_copy);
-        imwrite("Markers.jpg", src_copy);
-        // Perform the watershed algorithm
-        watershed(src, markers);
-        Mat mark = Mat::zeros(markers.size(), CV_8UC1);
-        markers.convertTo(mark, CV_8UC1);
-        bitwise_not(mark, mark);
-        //imshow("Markers_v2", mark); // uncomment this if you want to see how the mark
-                                      // image looks like at that point
-        // Generate random colors
-        vector<Vec3b> colors;
-        for (size_t i = 0; i < contours.size(); i++)
-        {
-            int b = 10 + 13*i;// theRNG().uniform(0, 255);
-            int g =  10 + 13*i;//theRNG().uniform(0, 255);
-            int r =  10 + 13*i;//theRNG().uniform(0, 255);
-            colors.push_back(Vec3b((uchar)b, (uchar)g, (uchar)r));
-        }
-        // Create the result image
-        Mat dst = Mat::zeros(markers.size(), CV_8UC3);
-        // Fill labeled objects with random colors
-        for (int i = 0; i < markers.rows; i++)
-        {
-            for (int j = 0; j < markers.cols; j++)
-            {
-                int index = markers.at<int>(i,j);
-                if (index > 0 && index <= static_cast<int>(contours.size()))
-                    dst.at<Vec3b>(i,j) = colors[index-1];
-                else
-                    dst.at<Vec3b>(i,j) = Vec3b(0,0,0);
-            }
-        }
-        // Visualize the final image
-        imshow("Final Result", dst);
-        imwrite("final.jpg", dst);
-        waitKey(10);
+        int b = 13 + 2*i;// theRNG().uniform(0, 255);
+        int g =  13 + 3*i;//theRNG().uniform(0, 255);
+        int r =  13 + 4*i;//theRNG().uniform(0, 255);
+        colors.push_back(Vec3b((uchar)b, (uchar)g, (uchar)r));
     }
+
+    _watershed(src, markers, dst, size, index_arr);
+    get_small_segments(dst, colors, index_arr, contours);
+    
+    Mat dst1;
+    for (int i = 0; i < contours.size(); ++i)
+    {
+        src_copy = dst.clone();
+        if(contourArea(contours[i]) > 250)
+        {
+            Mat markers(dst.rows, dst.cols, CV_8UC1, Scalar(0));
+            printf("%s %f\n","Area:", contourArea(contours[i]) );
+            Rect box =  boundingRect(contours[i]);
+            
+            /*Point p1,p2;
+            p1.x = box.tl().x - 5;
+            p1.y = box.tl().y - 5;
+            p2.x = box.br().x + 5;
+            p2.y = box.br().y + 5;
+            box.tl() = p1;
+            box.br() = p2;*/
+            Mat seg = src(box);
+            
+
+            rectangle( src_copy, box.tl(), box.br(), Scalar(0,0,255), 1, 8, 0 );
+            imshow("argva", seg);
+            imshow("aqw", src_copy);
+
+            waitKey(0);
+
+            get_markers(seg, markers, s1, c, alpha, beta, s, size, 0);
+            _watershed(seg, markers, dst1, size, index_arr);
+            
+
+        }
+    }
+   
+
+
+    printf("%d\n",contours.size() );
+
+
+    
+    waitKey(0);
+
     return 0;
     
 }
+
+
+/*
+apply sobel and circle also on small segments generated from watershed
+then match all the three*/
